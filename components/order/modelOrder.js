e352e0bbe6c014c71e4678a3e14b2f8eb2e02192
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-04 10:47:46 +0100

    correccion de la sintaxixs de la constante del modelOrder

--- components/order/modelOrder.js
+++ components/order/modelOrder.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
-const Movie = mongoose.model('Orders', new mongoose.Schema({
+const Order = mongoose.model('Orders', new mongoose.Schema({
     title: {type: String} //Parametro de la tabla titulo de la pelicula
 }));
 
